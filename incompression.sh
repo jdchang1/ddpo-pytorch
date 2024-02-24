c8d52f4a9577fe083a0206d565506296b079fc8d
Wenhao Zhan <wzhan@lee-gpu-3.ee.princeton.edu>
2024-02-24 00:44:27 +0000

    fix

--- incompression.sh
+++ incompression.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 29051 --num_processes 4 scripts/train.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 29058 scripts/train.py \
 	--config config/dgx.py:incompressibility \
 	--config.seed=42 \
 	--config.entity_name=diffusion_cornell \
 	--config.project_name=incompression_final \
 	--config.run_name=ddpo_incompression_42 \
 	--config.sample.num_batches_per_epoch=8 \
-	--config.train.gradient_accumulation_steps=4
+	--config.train.gradient_accumulation_steps=8 \
+	--config.train.batch_size=2
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 29051 --num_processes 4 scripts/train.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 29058 scripts/train.py \
 	--config config/dgx.py:incompressibility \
 	--config.seed=55513 \
 	--config.entity_name=diffusion_cornell \
 	--config.project_name=incompression_final \
 	--config.run_name=ddpo_incompression_55513 \
 	--config.sample.num_batches_per_epoch=8 \
-	--config.train.gradient_accumulation_steps=4
+	--config.train.gradient_accumulation_steps=8 \
+	--config.train.batch_size=2
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 29051 --num_processes 4 scripts/train.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 29058 scripts/train.py \
 	--config config/dgx.py:incompressibility \
 	--config.seed=66624 \
 	--config.entity_name=diffusion_cornell \
 	--config.project_name=incompression_final \
 	--config.run_name=ddpo_incompression_66624 \
 	--config.sample.num_batches_per_epoch=8 \
-	--config.train.gradient_accumulation_steps=4
+	--config.train.gradient_accumulation_steps=8 \
+	--config.train.batch_size=2
